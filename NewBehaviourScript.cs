351a54c39a7d255fe37c60e9edd483287e78a1ee
Nico Virgino <nicovirgino@gmail.com>
2019-12-13 15:11:28 -0600

    comming new behaviour scripts for unity object. first commit test.

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour //means inherets or extends to monobehavior
+{
+    // Start is called before the first frame update
+
+        //public(inside world can use the variable)
+        //or private only user knows variable exists reference
+        [SerializeField]
+        private float _speed = 5.5f;
+    void Start()
+    {
+       //take the current position - new position (0x, 0y, 0z)
+       transform.position = new Vector3(0, 0, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        //test comment for git
+        //anotha one
+        
+    }
+}
