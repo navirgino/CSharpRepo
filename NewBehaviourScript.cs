5767792c662cc3eb063b1ec847b4a3e4a458f40b
Nico Virgino <nicovirgino@gmail.com>
2019-12-13 15:38:38 -0600

    added some a "wrap" functionality on the x-axis, to make object wrap smoothly.

--- NewBehaviourScript.cs
+++ NewBehaviourScript.cs
@@ -27,7 +27,29 @@ public class NewBehaviourScript : MonoBehaviour //means inherets or extends to m
 
         transform.Translate(direction * _speed * Time.deltaTime);
 
-        //test comment for git
+        if(transform.position.y >= 0)
+        {
+            //making new vector object upon the transformatio when y is greater or equal to 0
+            transform.position = new Vector3(transform.position.x, 0, 0);
+
+
+        }else if(transform.position.y <= -3.8f)
+        {
+            //making new vector obj upon the transformation when x is less than or equal to -3.8f on the x axis
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+
+        }
+
+        //setting boundaries to wrap on the x axis!
+        if(transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }else if(transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+
+
         
         
     }
