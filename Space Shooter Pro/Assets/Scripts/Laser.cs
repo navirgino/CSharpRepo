568cde04133699b1d3f12f59ce41926ad059e305
Nico Virgino <nicovirgino@gmail.com>
2019-12-15 14:47:44 -0600

    added new script specifically for laser prefab, trying to work on movement upwards post key press

---
+++ Space Shooter Pro/Assets/Scripts/Laser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 10.5f;
+
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalcMovement(); 
+    }
+    void CalcMovement()
+    {
+       
+     if (Input.GetKeyDown(KeyCode.Space))
+        {
+     
+        }
+    }
+}
