8e4118ee1a4ec1a94201654bc232cfc63a8a8ad0
Nico Virgino <nicovirgino@gmail.com>
2019-12-28 19:11:13 -0600

    enemy respawn randomly on the x axis

--- Space Shooter Pro/Assets/Scripts/Enemy.cs
+++ Space Shooter Pro/Assets/Scripts/Enemy.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 4.0f;
+    [SerializeField]
+    private GameObject _enemyPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,25 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //move down at 4 meters per second
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        //if bottom of screen
+        if(transform.position.y < -5)
+        {
+            float randomX = Random.Range(-8f, 8f);
+            transform.position = new Vector3(randomX, 7, 0);
+          
+        }
+        //respawn at top
+
+        //if bottom of screen
+        //respawn at top with a new random x position
+    }
+    void respAtTop()
+    {
         
+        Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+
     }
 }
