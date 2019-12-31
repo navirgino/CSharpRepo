0531a8d7d1326b2d2577d236868bf8d41d5158f5
Nico Virgino <nicovirgino@gmail.com>
2019-12-30 18:05:17 -0600

    spawns continuously, but there is a bug where it spawns way more than it should. && created enemy container

---
+++ Space Shooter Pro/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+    //spawn game objects every 5 seconds
+    //create a coroutine of type IEnumerator -- yield events
+    //while loop to make it continuous
+    IEnumerator SpawnRoutine()
+    {
+  
+        while (true)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+
+            yield return new WaitForSeconds(5.0f);
+
+            Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        }
+    }
+
+}
