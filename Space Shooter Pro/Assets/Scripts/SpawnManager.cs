7694f815b96cea9a89162ab04f6a55c19131c577
Nico Virgino <nicovirgino@gmail.com>
2020-01-05 21:18:03 -0600

    fixing spawn bug

--- Space Shooter Pro/Assets/Scripts/SpawnManager.cs
+++ Space Shooter Pro/Assets/Scripts/SpawnManager.cs
@@ -24,11 +24,12 @@ public class SpawnManager : MonoBehaviour
     //while loop to make it continuous
     IEnumerator SpawnRoutine()
     {
-  
         while (true)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+
             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+
             newEnemy.transform.parent = _enemyContainer.transform;
 
             yield return new WaitForSeconds(5.0f);
